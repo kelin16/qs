25651f1a2fc131706ce077b9352bdbd6f919b99b
ke_lin10 <ke_lin10@126.com>
2017-11-18 12:01:33 +0800

    first

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,15 +4,34 @@ namespace Home\Controller;
 use Think\Controller;
 header("Content-type:text/html;charset=utf-8");
 class IndexController extends Controller {
-    public function header(){
-        $this -> display();
-    }
     public function content(){
         $this -> display();
     }
     public function index(){
         if(session('admin_name')){
-                $this -> display();
+            $role_id = session('admin_role');
+            $admin_id = session('admin_id');
+            $admin_name = session('admin_name');
+            if($admin_name == 'admin'){
+                $authinfoA = D('auth')
+                    ->where(array('auth_level'=>0))
+                    ->select();
+                $authinfoB = D('auth')
+                    ->where(array('auth_level'=>1))
+                    ->select();
+            }else{
+                $roleinfo = D('role')->find($role_id);
+                $authids = $roleinfo['role_auth_ids'];
+                $authinfoA = D('auth')
+                    ->where(array('auth_level'=>0,'auth_id'=>array('in',$authids)))
+                    ->select();
+                $authinfoB = D('auth')
+                    ->where(array('auth_level'=>1,'auth_id'=>array('in',$authids)))
+                    ->select();
+            }
+            $this ->assign('authinfoA',$authinfoA);
+            $this ->assign('authinfoB',$authinfoB);
+            $this -> display();
         }else{
             $this->redirect('user/login', array(), 0, "");
         }
